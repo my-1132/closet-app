befd7e3eb186c64713ce4682b636a3aede45bd4a
my-1132 <myk1350909@gmail.com>
2021-11-12 15:16:47 +0900

    googleログイン実装しました

---
+++ pages/login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="login">
+        <form class="form" @submit.prevent>
+            <div>ログイン</div>
+            <label class="label">
+                <div>メールアドレス</div>
+                <div><input type="text" v-model="email"></div>
+            </label>
+            <label class="label">
+                <div>パスワード</div>
+                <div><input type="text" v-model="pass"></div>
+            </label>
+            <div><router-link to="/news1"><button class="button" type="submit" @click="login1">ログイン</button></router-link></div>
+        </form>
+    </div>
+</template>
+
+<script>
+// import firebase from 'firebase/compat/app'
+
+export default {    
+    data(){
+        return{
+            email:'',
+            pass:''
+        }
+    },
+    methods:{
+        login1(email,pass){
+            this.$store.dispatch('login1', {email: this.email, pass: this.pass})        
+        }
+
+    }
+}
+</script>
