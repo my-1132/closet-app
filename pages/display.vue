eb6f949d7d552fc68429e47349fee780b2339fa0
haruna325 <kineharu25@gmail.com>
2021-11-12 10:05:54 +0900

    画像の追加

---
+++ pages/display.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="date">今日</div>
+        <span class="location">東京</span>
+        <p>{{ today }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+
+        }
+    },
+    computed: {
+        today(){
+        // 気象情報表示部分の今日の日付
+            const now = new  Date()
+            const week = ['日','月','火','水','木','金','土']
+            return `${now.getMonth()+1}/${now.getDate()}(${week[now.getDay()]}）`
+        }
+    }
+
+}
+</script>
+
